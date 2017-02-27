9a9f01a9adc1b07d0afb34d0cc58c7df4e338719
ipukhov <ivan.pukhov@cogniteq.com>
2017-02-27 19:00:14 +0300

    Job limits. Ignoring dublicates

--- src/jobLimits.js.js
+++ src/jobLimits.js.js
@@ -13,7 +13,7 @@ sc.Init({
 function isAdmin(expectedRoleId) {
     var isAdminRole = false;
     for(let i = 0; i < userRoles.length; i++) {
-        if (userRoles[i] === expectedRoleId) {
+        if (userRoles[i] === expectedRoleId || userRoles[i] === '*') {
             isAdminRole = true;
             break;
         }
@@ -21,11 +21,19 @@ function isAdmin(expectedRoleId) {
     return isAdminRole
 }
 
+function getStoredJobLimits(ids) {
+    const q = new sc.Query('obj08_3');
+    q.equalTo('obj08', pool.obj08);
+    q.containedIn('ref03', ids);
+    return q.find();
+}
+
+
 //TODO: Удалить, когда добавится нужная логика на UI.
 pool.data = [];
-pool.ids.map(id => {
+pool.ids.map(limId => {
     pool.data.push({
-        id: id,
+        id: limId,
         limitCost: 12,
         isRemoved: false
     })
@@ -37,23 +45,30 @@ query.equalTo("code", ADMIN_ROLE)
 query.find().then(res => {
     let adminRole = res.result;
     if (adminRole.length > 0) {
+        if (isAdmin(adminRole[0]._id)) {
 
-        if (isAdmin(adminRole[0].get("_id"))) {
             if (pool.cmd === 'add') {
-                const array = pool.data.map(jobLimit => {
-                        return new sc.Object('obj08_3')
-                            .set('obj08', pool.obj08)
-                            .set('ref03', jobLimit.id)
-                            .set('isRemoved', jobLimit.isRemoved)
-                            .set('limitCost', jobLimit.limitCost)
-                            .save();
-                });
+                let jobLimitIds = pool.data.map(data => data.id);
+                getStoredJobLimits(jobLimitIds).then(res => {
+                    let storedIds = res.result.map(storedLimit => storedLimit.ref03);
+                    const array = pool.data.map(jobLimit => {
+                            if(!storedIds.find(id => id === jobLimit.id)) {
+                                return new sc.Object('obj08_3')
+                                    .set('obj08', pool.obj08)
+                                    .set('ref03', jobLimit.id)
+                                    .set('isRemoved', jobLimit.isRemoved)
+                                    .set('limitCost', jobLimit.limitCost)
+                                    .save();
+                            }
+                    });
                 Promise.all(array)
-                .then(res => {
+                    .then(res => {
                     response(res)
                 }).catch(e => {
                     response(e)
                 });
+                }).catch(e => response(e)) ;
+
             } else {
                 const q = new sc.Query('obj08_3');
                 q.equalTo('obj08', pool.obj08);
@@ -76,48 +91,10 @@ query.find().then(res => {
         response({
             "error": true,
             "errCode": 204,
-            "errMsg": "В базе нет роли admin"
+            "errMsg": "В базе нет роли " + ADMIN_ROLE
         })
     }
 }).catch(e => {
     response(e)
 });
 
-
-if (userRoles.find(role => role === 'KVPPu9oWaD')) {
-    if (pool.cmd === 'add') {
-        const array = pool.data.map(jobLimit => {
-                return new sc.Object('obj08_3')
-                    .set('obj08', pool.obj08)
-                    .set('ref03', jobLimit.id)
-                    .set('isRemoved', jobLimit.isRemoved)
-                    .set('limitCost', jobLimit.limitCost)
-                    .save();
-    })
-
-        Promise.all(array)
-            .then(res => {
-            response(res)
-        })
-    .catch(e => {
-            response(e)
-        });
-    } else {
-        const q = new sc.Query('obj08_3');
-        q.equalTo('obj08', pool.obj08);
-        q.containedIn('_id', pool.ids);
-        q.remove()
-            .then(res => {
-            response(res)
-        })
-    .catch(e => {
-            response(e)
-        })
-    }
-} else {
-    response({
-        "error": true,
-        "errCode": 403,
-        "errMsg": "Только администратор может удалить или добавить лимит"
-    })
-}
